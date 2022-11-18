b2d7f3341012e6b11955748ccef5913c0dc56cab
morciu <marianpralea@gmail.com>
2022-11-18 16:38:05 +0200

    created priority radio input

--- src/components/newTodoForm.js
+++ src/components/newTodoForm.js
@@ -1,12 +1,14 @@
 import dateInput from "./dateInput";
 import textInput from "./textInput";
+import priorityInput from "./priorityInput";
 
 export default function newTodoForm() {
     const element = document.createElement('div');
 
     element.appendChild(textInput("Title", "title"));
     element.appendChild(textInput("Description", "description"));
-    element.appendChild(dateInput("Date", "date"));
+    element.appendChild(dateInput("Due Date", "date"));
+    element.appendChild(priorityInput());
 
     return element;
 };
---
+++ src/components/priorityInput.js
@@ -0,0 +1,50 @@
+export default function priorityInput() {
+    const element = document.createElement("div");
+
+    const elementLabel = document.createElement("span");
+    elementLabel.innerText = "Priority";
+
+    // Low Priority
+    const lowLabel = document.createElement("label");
+    lowLabel.setAttribute("for", "lowPriority");
+    lowLabel.innerText = "Low";
+
+    const lowInput = document.createElement("input");
+    lowInput.type = "radio";
+    lowInput.id = "lowPriority";
+    lowInput.name = "priority";
+
+    lowLabel.appendChild(lowInput);
+
+    // Medium Priority
+    const mediumLabel = document.createElement("label");
+    mediumLabel.setAttribute("for", "mediumPriority");
+    mediumLabel.innerText = "Medium";
+
+    const mediumInput = document.createElement("input");
+    mediumInput.type = "radio";
+    mediumInput.id = "mediumPriority";
+    mediumInput.name = "priority";
+
+    mediumLabel.appendChild(mediumInput);
+
+    // High Priority
+    const highLabel = document.createElement("label");
+    highLabel.setAttribute("for", "highPriority");
+    highLabel.innerText = "High";
+
+    const highInput = document.createElement("input");
+    highInput.type = "radio";
+    highInput.id = "highPriority";
+    highInput.name = "priority";
+
+    highLabel.appendChild(highInput);
+
+    // Append to main div
+    element.appendChild(elementLabel);
+    element.appendChild(lowLabel);
+    element.appendChild(mediumLabel);
+    element.appendChild(highLabel);
+
+    return element;
+};
